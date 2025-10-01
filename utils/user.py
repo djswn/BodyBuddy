231458a21884437242f972e6e0fcd07c70e97304
cjww543210 <cjww543210@gmail.com>
2025-10-01 19:24:03 +0900

    Add files via upload

---
+++ utils/user.py
@@ -0,0 +1,16 @@
+# utils/user.py
+
+def make_user_info(name, age, height, weight, body_fat, target_weight, diet_period_weeks):
+    """
+    전달받은 사용자 정보를 딕셔너리로 반환하는 함수
+    """
+    user_info = {
+        "name": name,
+        "age": age,
+        "height": height,
+        "weight": weight,
+        "body_fat": body_fat,
+        "target_weight": target_weight,
+        "diet_period_weeks": diet_period_weeks
+    }
+    return user_info
