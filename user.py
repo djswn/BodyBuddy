58e444372cb3951c724445633860eea7d4c533ee
JM030220 <beautyboy5924@naver.com>
2025-10-29 19:23:53 +0900

    [refactor] JSON을 활용한 사용자 정보 영구 저장
    회원가입을 하고 로그인을 하고 사용자 정보를 입력해도, 어플을 종료시키면 모든 정보가 다 날아가 버렸음
    JSON을 추가하고, user.py 코드를 약간 수정하여 한 번 저장한 사용자의 정보는 어플을 껐다가 다시 실행하여도 남아있게끔 만듦

---
+++ user.py
@@ -0,0 +1,11 @@
+def make_user_info(name, age, height, weight, body_fat, target_weight, diet_period_weeks, gender):
+    return {
+        "name": name,
+        "age": age,
+        "height": height,
+        "weight": weight,
+        "body_fat": body_fat,
+        "target_weight": target_weight,
+        "diet_period_weeks": diet_period_weeks,
+        "gender": gender  
+    }
